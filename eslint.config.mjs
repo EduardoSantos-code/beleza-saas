9337849c66b0ec657951919e1f864d53b72befc8
Eduardo Santos <eduardo.santos2097@hotmail.com>
2026-05-11 20:12:57 -0300

    add club foundation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,14 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "react-hooks/set-state-in-effect": "off",
+      "react/no-unescaped-entities": "off",
+      "prefer-const": "warn"
+    }
+  }
 ]);
 
 export default eslintConfig;
